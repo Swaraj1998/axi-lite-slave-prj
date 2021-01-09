61ca1f40e4d6848ddfb1e86dc3af83b17d4fe2c0
Swaraj Hota <swarajhota353@gmail.com>
2021-01-09 20:45:57 +0530

    fix indentation

--- axi_lite_slave.vhd
+++ axi_lite_slave.vhd
@@ -3,262 +3,262 @@ use IEEE.std_logic_1164.ALL;
 use iEEE.numeric_std.ALL;
 
 entity axi_lite_slave is
-	generic (
-		C_S_AXI_DATA_WIDTH : natural := 32;
-		C_S_AXI_ADDR_WIDTH : natural := 32
-	);
-	port (
-		s_axi_aclk		: in std_logic;
-		s_axi_areset_n	: in std_logic;
+    generic (
+        C_S_AXI_DATA_WIDTH : natural := 32;
+        C_S_AXI_ADDR_WIDTH : natural := 32
+    );
+    port (
+        s_axi_aclk      : in std_logic;
+        s_axi_areset_n  : in std_logic;
 
-		s_axi_awaddr	: in std_logic_vector
-			(C_S_AXI_ADDR_WIDTH-1 downto 0); 
-		s_axi_awprot	: in std_logic_vector (2 downto 0);
-		s_axi_awvalid	: in std_logic;
-		s_axi_awready	: out std_logic;
-		s_axi_wdata		: in std_logic_vector
-			(C_S_AXI_DATA_WIDTH-1 downto 0);
-		s_axi_wstrb		: in std_logic_vector
-			((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
-		s_axi_wvalid	: in std_logic;
-		s_axi_wready	: out std_logic;
-		s_axi_bresp		: out std_logic_vector (1 downto 0);
-		s_axi_bvalid	: out std_logic;
-		s_axi_bready	: in std_logic;
+        s_axi_awaddr    : in std_logic_vector
+            (C_S_AXI_ADDR_WIDTH-1 downto 0); 
+        s_axi_awprot    : in std_logic_vector (2 downto 0);
+        s_axi_awvalid   : in std_logic;
+        s_axi_awready   : out std_logic;
+        s_axi_wdata     : in std_logic_vector
+            (C_S_AXI_DATA_WIDTH-1 downto 0);
+        s_axi_wstrb     : in std_logic_vector
+            ((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+        s_axi_wvalid    : in std_logic;
+        s_axi_wready    : out std_logic;
+        s_axi_bresp     : out std_logic_vector (1 downto 0);
+        s_axi_bvalid    : out std_logic;
+        s_axi_bready    : in std_logic;
 
-		s_axi_araddr	: in std_logic_vector
-			(C_S_AXI_ADDR_WIDTH-1 downto 0);
-		s_axi_arprot	: in std_logic_vector (2 downto 0);
-		s_axi_arvalid	: in std_logic;
-		s_axi_arready	: out std_logic;
-		s_axi_rdata		: out std_logic_vector
-			(C_S_AXI_DATA_WIDTH-1 downto 0);
-		s_axi_rresp		: out std_logic_vector (1 downto 0);
-		s_axi_rvalid	: out std_logic;
-		s_axi_rready	: in std_logic
-	);
+        s_axi_araddr    : in std_logic_vector
+            (C_S_AXI_ADDR_WIDTH-1 downto 0);
+        s_axi_arprot    : in std_logic_vector (2 downto 0);
+        s_axi_arvalid   : in std_logic;
+        s_axi_arready   : out std_logic;
+        s_axi_rdata     : out std_logic_vector
+            (C_S_AXI_DATA_WIDTH-1 downto 0);
+        s_axi_rresp     : out std_logic_vector (1 downto 0);
+        s_axi_rvalid    : out std_logic;
+        s_axi_rready    : in std_logic
+    );
 end entity axi_lite_slave;
 
 architecture RTL of axi_lite_slave is
-	signal axi_awready	: std_logic := '1';
-	signal axi_wready	: std_logic := '1';
-	signal axi_bvalid	: std_logic := '0';
-	signal axi_arready	: std_logic := '0';
-	signal axi_rvalid	: std_logic := '0';
-	signal axi_rdata : std_logic_vector (C_S_AXI_DATA_WIDTH-1 downto 0);
+    signal axi_awready  : std_logic := '1';
+    signal axi_wready   : std_logic := '1';
+    signal axi_bvalid   : std_logic := '0';
+    signal axi_arready  : std_logic := '0';
+    signal axi_rvalid   : std_logic := '0';
+    signal axi_rdata : std_logic_vector (C_S_AXI_DATA_WIDTH-1 downto 0);
 
-	constant ADDR_LSB : natural := 2;
-	constant AW : natural := C_S_AXI_ADDR_WIDTH-2;
-	constant DW : natural := C_S_AXI_DATA_WIDTH;
+    constant ADDR_LSB : natural := 2;
+    constant AW : natural := C_S_AXI_ADDR_WIDTH-2;
+    constant DW : natural := C_S_AXI_DATA_WIDTH;
 
-	-- User registers
-	type reg_file is array (0 to 63) of std_logic_vector (DW-1 downto 0);
-	signal slv_mem : reg_file;
+    -- User registers
+    type reg_file is array (0 to 63) of std_logic_vector (DW-1 downto 0);
+    signal slv_mem : reg_file;
 
-	-- Buffers for read process
-	signal pre_raddr : std_logic_vector
-		(C_S_AXI_ADDR_WIDTH-1 downto 0);
-	signal rd_addr  : std_logic_vector
-		(C_S_AXI_ADDR_WIDTH-1 downto 0);
+    -- Buffers for read process
+    signal pre_raddr : std_logic_vector
+        (C_S_AXI_ADDR_WIDTH-1 downto 0);
+    signal rd_addr  : std_logic_vector
+        (C_S_AXI_ADDR_WIDTH-1 downto 0);
 
-	-- Buffers for write process
-	signal pre_waddr : std_logic_vector
-		(C_S_AXI_ADDR_WIDTH-1 downto 0);
-	signal waddr : std_logic_vector
-		(C_S_AXI_ADDR_WIDTH-1 downto 0);
+    -- Buffers for write process
+    signal pre_waddr : std_logic_vector
+        (C_S_AXI_ADDR_WIDTH-1 downto 0);
+    signal waddr : std_logic_vector
+        (C_S_AXI_ADDR_WIDTH-1 downto 0);
 
-	signal pre_wdata : std_logic_vector
-		(C_S_AXI_DATA_WIDTH-1 downto 0);
-	signal wdata : std_logic_vector
-		(C_S_AXI_DATA_WIDTH-1 downto 0);
+    signal pre_wdata : std_logic_vector
+        (C_S_AXI_DATA_WIDTH-1 downto 0);
+    signal wdata : std_logic_vector
+        (C_S_AXI_DATA_WIDTH-1 downto 0);
 
-	signal pre_wstrb : std_logic_vector
-		((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
-	signal wstrb : std_logic_vector
-		((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+    signal pre_wstrb : std_logic_vector
+        ((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+    signal wstrb : std_logic_vector
+        ((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
 
-	-- Some useful indicators
-	signal valid_read_request	: std_logic;
-	signal read_response_stall	: std_logic;
+    -- Some useful indicators
+    signal valid_read_request   : std_logic;
+    signal read_response_stall  : std_logic;
 
-	signal valid_write_address	: std_logic;
-	signal valid_write_data		: std_logic;
-	signal write_response_stall	: std_logic;
+    signal valid_write_address  : std_logic;
+    signal valid_write_data     : std_logic;
+    signal write_response_stall : std_logic;
 
 begin
-	s_axi_awready	<= axi_awready;
-	s_axi_wready	<= axi_wready;
-	s_axi_bresp	<= "00";	-- OKAY response
-	s_axi_bvalid	<= axi_bvalid;
-	s_axi_arready	<= axi_arready;
-	s_axi_rdata	<= axi_rdata;
-	s_axi_rresp	<= "00";	-- OKAY response
-	s_axi_rvalid	<= axi_rvalid;
+    s_axi_awready   <= axi_awready;
+    s_axi_wready    <= axi_wready;
+    s_axi_bresp <= "00";    -- OKAY response
+    s_axi_bvalid    <= axi_bvalid;
+    s_axi_arready   <= axi_arready;
+    s_axi_rdata <= axi_rdata;
+    s_axi_rresp <= "00";    -- OKAY response
+    s_axi_rvalid    <= axi_rvalid;
 
-	valid_read_request   <= s_axi_arvalid or not axi_arready;
-	read_response_stall  <= axi_rvalid and not s_axi_rready;
+    valid_read_request   <= s_axi_arvalid or not axi_arready;
+    read_response_stall  <= axi_rvalid and not s_axi_rready;
 
-	valid_write_address  <= s_axi_awvalid or not axi_awready;
-	valid_write_data     <= s_axi_wvalid or not axi_wready;
-	write_response_stall <= axi_bvalid and not s_axi_bready;
+    valid_write_address  <= s_axi_awvalid or not axi_awready;
+    valid_write_data     <= s_axi_wvalid or not axi_wready;
+    write_response_stall <= axi_bvalid and not s_axi_bready;
 
-	---- Read Processing ----
+    ---- Read Processing ----
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- The read response channel valid signal
-			if s_axi_areset_n = '0' then
-				axi_rvalid <= '0';
-			elsif read_response_stall = '1' then
-				axi_rvalid <= '1';
-			elsif valid_read_request = '1' then
-				axi_rvalid <= '1';
-			else
-				axi_rvalid <= '0';
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- The read response channel valid signal
+            if s_axi_areset_n = '0' then
+                axi_rvalid <= '0';
+            elsif read_response_stall = '1' then
+                axi_rvalid <= '1';
+            elsif valid_read_request = '1' then
+                axi_rvalid <= '1';
+            else
+                axi_rvalid <= '0';
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- Buffer the address
-			if axi_arready = '1' then
-				pre_raddr <= s_axi_araddr;
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- Buffer the address
+            if axi_arready = '1' then
+                pre_raddr <= s_axi_araddr;
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- Read the data
-			if read_response_stall = '0'
-			and valid_read_request = '1' then
-				axi_rdata <= slv_mem(to_integer(unsigned(rd_addr(AW+ADDR_LSB-1 downto ADDR_LSB))));
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- Read the data
+            if read_response_stall = '0'
+            and valid_read_request = '1' then
+                axi_rdata <= slv_mem(to_integer(unsigned(rd_addr(AW+ADDR_LSB-1 downto ADDR_LSB))));
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- The read address channel ready signal
-			if s_axi_areset_n = '0' then
-				axi_arready <= '1';
-			elsif read_response_stall = '1' then
-				axi_arready <= not valid_read_request;
-			else
-				axi_arready <= '1';
-			end if;
-		end if;
-	end process;
-	
-	rd_addr <= pre_raddr when axi_arready = '0' else s_axi_araddr;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- The read address channel ready signal
+            if s_axi_areset_n = '0' then
+                axi_arready <= '1';
+            elsif read_response_stall = '1' then
+                axi_arready <= not valid_read_request;
+            else
+                axi_arready <= '1';
+            end if;
+        end if;
+    end process;
+    
+    rd_addr <= pre_raddr when axi_arready = '0' else s_axi_araddr;
 
-	---- Write Processing ----
+    ---- Write Processing ----
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- The write address channel ready signal
-			if s_axi_areset_n = '0' then
-				axi_awready <= '1';
-			elsif write_response_stall = '1' then
-				axi_awready <= not valid_write_address;
-			elsif valid_write_data = '1' then
-				axi_awready <= '1';
-			else
-				axi_awready <= axi_awready and not s_axi_awvalid; 
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- The write address channel ready signal
+            if s_axi_areset_n = '0' then
+                axi_awready <= '1';
+            elsif write_response_stall = '1' then
+                axi_awready <= not valid_write_address;
+            elsif valid_write_data = '1' then
+                axi_awready <= '1';
+            else
+                axi_awready <= axi_awready and not s_axi_awvalid; 
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- The write data channel ready signal
-			if s_axi_areset_n = '0' then
-				axi_wready <= '1';
-			elsif write_response_stall = '1' then
-				axi_wready <= not valid_write_data;
-			elsif valid_write_address = '1' then
-				axi_wready <= '1';
-			else
-				axi_wready <= axi_wready and not s_axi_wvalid;
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- The write data channel ready signal
+            if s_axi_areset_n = '0' then
+                axi_wready <= '1';
+            elsif write_response_stall = '1' then
+                axi_wready <= not valid_write_data;
+            elsif valid_write_address = '1' then
+                axi_wready <= '1';
+            else
+                axi_wready <= axi_wready and not s_axi_wvalid;
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- Buffer the address
-			if axi_awready = '1' then
-				pre_waddr <= s_axi_awaddr;
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- Buffer the address
+            if axi_awready = '1' then
+                pre_waddr <= s_axi_awaddr;
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- Buffer the data
-			if axi_wready = '1' then
-				pre_wdata <= s_axi_wdata;
-				pre_wstrb <= s_axi_wstrb;
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- Buffer the data
+            if axi_wready = '1' then
+                pre_wdata <= s_axi_wdata;
+                pre_wstrb <= s_axi_wstrb;
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- Actually write the data
-			if write_response_stall = '0'
-			and valid_write_address = '1'
-			and valid_write_data = '1' then
-				if wstrb(0) = '1' then
-					slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
-						(7 downto 0) <= wdata(7 downto 0);
-				end if;
-				if wstrb(1) = '1' then
-					slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
-						(15 downto 8) <= wdata(15 downto 8);
-				end if;
-				if wstrb(2) = '1' then
-					slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
-						(23 downto 16) <= wdata(23 downto 16);
-				end if;
-				if wstrb(3) = '1' then
-					slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
-						(31 downto 24) <= wdata(31 downto 24);
-				end if;
-			end if;
-		end if;
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- Actually write the data
+            if write_response_stall = '0'
+            and valid_write_address = '1'
+            and valid_write_data = '1' then
+                if wstrb(0) = '1' then
+                    slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
+                        (7 downto 0) <= wdata(7 downto 0);
+                end if;
+                if wstrb(1) = '1' then
+                    slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
+                        (15 downto 8) <= wdata(15 downto 8);
+                end if;
+                if wstrb(2) = '1' then
+                    slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
+                        (23 downto 16) <= wdata(23 downto 16);
+                end if;
+                if wstrb(3) = '1' then
+                    slv_mem(to_integer(unsigned(waddr(AW+ADDR_LSB-1 downto ADDR_LSB))))
+                        (31 downto 24) <= wdata(31 downto 24);
+                end if;
+            end if;
+        end if;
+    end process;
 
-	process (s_axi_aclk) is
-	begin
-		if rising_edge(s_axi_aclk) then
-			-- The write response channel valid signal
-			if s_axi_areset_n = '0' then
-				axi_bvalid <= '0';
-			elsif valid_write_address = '1'
-			and valid_write_data = '1' then
-				axi_bvalid <= '1';
-			elsif s_axi_bready = '1' then
-				axi_bvalid <= '0';
-			end if;
-		end if; 
-	end process;
+    process (s_axi_aclk) is
+    begin
+        if rising_edge(s_axi_aclk) then
+            -- The write response channel valid signal
+            if s_axi_areset_n = '0' then
+                axi_bvalid <= '0';
+            elsif valid_write_address = '1'
+            and valid_write_data = '1' then
+                axi_bvalid <= '1';
+            elsif s_axi_bready = '1' then
+                axi_bvalid <= '0';
+            end if;
+        end if; 
+    end process;
 
-	-- Read write address from buffer
-	waddr <= pre_waddr when axi_awready = '0' else s_axi_awaddr;
+    -- Read write address from buffer
+    waddr <= pre_waddr when axi_awready = '0' else s_axi_awaddr;
 
-	-- Read write data from buffer
-	wstrb <= pre_wstrb when axi_wready = '0' else s_axi_wstrb;
-	wdata <= pre_wdata when axi_wready = '0' else s_axi_wdata;
+    -- Read write data from buffer
+    wstrb <= pre_wstrb when axi_wready = '0' else s_axi_wstrb;
+    wdata <= pre_wdata when axi_wready = '0' else s_axi_wdata;
 
 end architecture RTL;
--- top.vhd
+++ top.vhd
@@ -1,14 +1,14 @@
 ----------------------------------------------------------------------------
 --  top.vhd
---	MicroZed AXI Lite Slave (with user registers)
---	Version 1.0
+--  MicroZed AXI Lite Slave (with user registers)
+--  Version 1.0
 --
 --  Copyright (C) 2021 Swaraj Hota
 --
---	This program is free software: you can redistribute it and/or
---	modify it under the terms of the GNU General Public License
---	as published by the Free Software Foundation, either version
---	2 of the License, or (at your option) any later version.
+--  This program is free software: you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation, either version
+--  2 of the License, or (at your option) any later version.
 --
 --  Vivado 2020.2:
 --    mkdir -p build.vivado
@@ -24,7 +24,7 @@ use IEEE.numeric_std.ALL;
 library unisim;
 use unisim.VCOMPONENTS.ALL;
 
-use work.vivado_pkg.ALL;	-- Vivado Attributes
+use work.vivado_pkg.ALL;    -- Vivado Attributes
 
 use work.axi3m_pkg.ALL;
 use work.axi3ml_pkg.ALL;
@@ -58,107 +58,107 @@ architecture RTL of top is
 
 begin
 
-	m_axi0_aclk <= ps_fclk(0);
+    m_axi0_aclk <= ps_fclk(0);
 
     --------------------------------------------------------------------
     -- PS7 Interface
     --------------------------------------------------------------------
 
     ps7_stub_inst : entity work.ps7_stub
-	port map (
-		ps_fclk => ps_fclk,
-		ps_reset_n => ps_reset_n,
-		--
-		m_axi0_aclk => m_axi0_aclk,
-		m_axi0_areset_n => m_axi0_areset_n,
-		--
-		m_axi0_arid => m_axi0_ro.arid,
-		m_axi0_araddr => m_axi0_ro.araddr,
-		m_axi0_arburst => m_axi0_ro.arburst,
-		m_axi0_arlen => m_axi0_ro.arlen,
-		m_axi0_arsize => m_axi0_ro.arsize,
-		m_axi0_arprot => m_axi0_ro.arprot,
-		m_axi0_arvalid => m_axi0_ro.arvalid,
-		m_axi0_arready => m_axi0_ri.arready,
-		--
-		m_axi0_rid => m_axi0_ri.rid,
-		m_axi0_rdata => m_axi0_ri.rdata,
-		m_axi0_rlast => m_axi0_ri.rlast,
-		m_axi0_rresp => m_axi0_ri.rresp,
-		m_axi0_rvalid => m_axi0_ri.rvalid,
-		m_axi0_rready => m_axi0_ro.rready,
-		--
-		m_axi0_awid => m_axi0_wo.awid,
-		m_axi0_awaddr => m_axi0_wo.awaddr,
-		m_axi0_awburst => m_axi0_wo.awburst,
-		m_axi0_awlen => m_axi0_wo.awlen,
-		m_axi0_awsize => m_axi0_wo.awsize,
-		m_axi0_awprot => m_axi0_wo.awprot,
-		m_axi0_awvalid => m_axi0_wo.awvalid,
-		m_axi0_awready => m_axi0_wi.awready,
-		--
-		m_axi0_wid => m_axi0_wo.wid,
-		m_axi0_wdata => m_axi0_wo.wdata,
-		m_axi0_wstrb => m_axi0_wo.wstrb,
-		m_axi0_wlast => m_axi0_wo.wlast,
-		m_axi0_wvalid => m_axi0_wo.wvalid,
-		m_axi0_wready => m_axi0_wi.wready,
-		--
-		m_axi0_bid => m_axi0_wi.bid,
-		m_axi0_bresp => m_axi0_wi.bresp,
-		m_axi0_bvalid => m_axi0_wi.bvalid,
-		m_axi0_bready => m_axi0_wo.bready );
+    port map (
+        ps_fclk => ps_fclk,
+        ps_reset_n => ps_reset_n,
+        --
+        m_axi0_aclk => m_axi0_aclk,
+        m_axi0_areset_n => m_axi0_areset_n,
+        --
+        m_axi0_arid => m_axi0_ro.arid,
+        m_axi0_araddr => m_axi0_ro.araddr,
+        m_axi0_arburst => m_axi0_ro.arburst,
+        m_axi0_arlen => m_axi0_ro.arlen,
+        m_axi0_arsize => m_axi0_ro.arsize,
+        m_axi0_arprot => m_axi0_ro.arprot,
+        m_axi0_arvalid => m_axi0_ro.arvalid,
+        m_axi0_arready => m_axi0_ri.arready,
+        --
+        m_axi0_rid => m_axi0_ri.rid,
+        m_axi0_rdata => m_axi0_ri.rdata,
+        m_axi0_rlast => m_axi0_ri.rlast,
+        m_axi0_rresp => m_axi0_ri.rresp,
+        m_axi0_rvalid => m_axi0_ri.rvalid,
+        m_axi0_rready => m_axi0_ro.rready,
+        --
+        m_axi0_awid => m_axi0_wo.awid,
+        m_axi0_awaddr => m_axi0_wo.awaddr,
+        m_axi0_awburst => m_axi0_wo.awburst,
+        m_axi0_awlen => m_axi0_wo.awlen,
+        m_axi0_awsize => m_axi0_wo.awsize,
+        m_axi0_awprot => m_axi0_wo.awprot,
+        m_axi0_awvalid => m_axi0_wo.awvalid,
+        m_axi0_awready => m_axi0_wi.awready,
+        --
+        m_axi0_wid => m_axi0_wo.wid,
+        m_axi0_wdata => m_axi0_wo.wdata,
+        m_axi0_wstrb => m_axi0_wo.wstrb,
+        m_axi0_wlast => m_axi0_wo.wlast,
+        m_axi0_wvalid => m_axi0_wo.wvalid,
+        m_axi0_wready => m_axi0_wi.wready,
+        --
+        m_axi0_bid => m_axi0_wi.bid,
+        m_axi0_bresp => m_axi0_wi.bresp,
+        m_axi0_bvalid => m_axi0_wi.bvalid,
+        m_axi0_bready => m_axi0_wo.bready );
 
     --------------------------------------------------------------------
     -- AXI Lite Mapper
     --------------------------------------------------------------------
 
     axi_lite_inst : entity work.axi_lite
-	port map (
-		s_axi_aclk => m_axi0_aclk,
-		s_axi_areset_n => m_axi0_areset_n,
-		
-		s_axi_ro => m_axi0_ri,
-		s_axi_ri => m_axi0_ro,
-		s_axi_wo => m_axi0_wi,
-		s_axi_wi => m_axi0_wo,
+    port map (
+        s_axi_aclk => m_axi0_aclk,
+        s_axi_areset_n => m_axi0_areset_n,
+        
+        s_axi_ro => m_axi0_ri,
+        s_axi_ri => m_axi0_ro,
+        s_axi_wo => m_axi0_wi,
+        s_axi_wi => m_axi0_wo,
 
-		m_axi_ro => m_axi0l_ro,
-		m_axi_ri => m_axi0l_ri,
-		m_axi_wo => m_axi0l_wo,
-		m_axi_wi => m_axi0l_wi );
+        m_axi_ro => m_axi0l_ro,
+        m_axi_ri => m_axi0l_ri,
+        m_axi_wo => m_axi0l_wo,
+        m_axi_wi => m_axi0l_wi );
 
     --------------------------------------------------------------------
     -- AXI Lite Slave
     --------------------------------------------------------------------
 
     axi_lite_slave_inst : entity work.axi_lite_slave
-	port map (
-		s_axi_aclk => m_axi0_aclk,	
-		s_axi_areset_n => m_axi0_areset_n,
-		--
-		s_axi_araddr => m_axi0_ro.araddr,
-		s_axi_arprot => m_axi0_ro.arprot,
-		s_axi_arvalid => m_axi0_ro.arvalid,
-		s_axi_arready => m_axi0_ri.arready,
-		--
-		s_axi_rdata => m_axi0_ri.rdata,
-		s_axi_rresp => m_axi0_ri.rresp,
-		s_axi_rvalid => m_axi0_ri.rvalid,
-		s_axi_rready => m_axi0_ro.rready,
-		--
-		s_axi_awaddr => m_axi0_wo.awaddr,
-		s_axi_awprot => m_axi0_wo.awprot,
-		s_axi_awvalid => m_axi0_wo.awvalid,
-		s_axi_awready => m_axi0_wi.awready,
-		--
-		s_axi_wdata => m_axi0_wo.wdata,
-		s_axi_wstrb => m_axi0_wo.wstrb,
-		s_axi_wvalid => m_axi0_wo.wvalid,
-		s_axi_wready => m_axi0_wi.wready,
-		--
-		s_axi_bresp => m_axi0_wi.bresp,
-		s_axi_bvalid => m_axi0_wi.bvalid,
-		s_axi_bready => m_axi0_wo.bready );
+    port map (
+        s_axi_aclk => m_axi0_aclk,  
+        s_axi_areset_n => m_axi0_areset_n,
+        --
+        s_axi_araddr => m_axi0_ro.araddr,
+        s_axi_arprot => m_axi0_ro.arprot,
+        s_axi_arvalid => m_axi0_ro.arvalid,
+        s_axi_arready => m_axi0_ri.arready,
+        --
+        s_axi_rdata => m_axi0_ri.rdata,
+        s_axi_rresp => m_axi0_ri.rresp,
+        s_axi_rvalid => m_axi0_ri.rvalid,
+        s_axi_rready => m_axi0_ro.rready,
+        --
+        s_axi_awaddr => m_axi0_wo.awaddr,
+        s_axi_awprot => m_axi0_wo.awprot,
+        s_axi_awvalid => m_axi0_wo.awvalid,
+        s_axi_awready => m_axi0_wi.awready,
+        --
+        s_axi_wdata => m_axi0_wo.wdata,
+        s_axi_wstrb => m_axi0_wo.wstrb,
+        s_axi_wvalid => m_axi0_wo.wvalid,
+        s_axi_wready => m_axi0_wi.wready,
+        --
+        s_axi_bresp => m_axi0_wi.bresp,
+        s_axi_bvalid => m_axi0_wi.bvalid,
+        s_axi_bready => m_axi0_wo.bready );
 
 end RTL;
